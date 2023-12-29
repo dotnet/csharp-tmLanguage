1f32fbfcede807edba05b1fe8b6008fa613f8504
Joey Robichaud <jorobich@microsoft.com>
2023-12-29 13:19:24 -0800

    Add support for scoped ref modifier

--- test/method.tests.ts
+++ test/method.tests.ts
@@ -796,6 +796,67 @@ public void LinearRegression(double[,] samples, double[] standardDeviations, int
             ]);
         });
 
+        it("ref parameter in parameter list", async () => {
+            const input = Input.InClass(`
+public static Span<T> CreateSpan<T>(ref T reference, int length) {}
+`);
+            const tokens = await tokenize(input);
+
+            tokens.should.deep.equal([
+                Token.Keyword.Modifier.Public,
+                Token.Keyword.Modifier.Static,
+                Token.Type("Span"),
+                Token.Punctuation.TypeParameter.Begin,
+                Token.Type("T"),
+                Token.Punctuation.TypeParameter.End,
+                Token.Identifier.MethodName("CreateSpan"),
+                Token.Punctuation.TypeParameter.Begin,
+                Token.Identifier.TypeParameterName("T"),
+                Token.Punctuation.TypeParameter.End,
+                Token.Punctuation.OpenParen,
+                Token.Keyword.Modifier.Ref,
+                Token.Type("T"),
+                Token.Identifier.ParameterName("reference"),
+                Token.Punctuation.Comma,
+                Token.PrimitiveType.Int,
+                Token.Identifier.ParameterName("length"),
+                Token.Punctuation.CloseParen,
+                Token.Punctuation.OpenBrace,
+                Token.Punctuation.CloseBrace
+            ]);
+        });
+
+        it("scoped ref parameter in parameter list (#306)", async () => {
+            const input = Input.InClass(`
+public static Span<T> CreateSpan<T>(scoped ref T reference, int length) {}
+`);
+            const tokens = await tokenize(input);
+
+            tokens.should.deep.equal([
+                Token.Keyword.Modifier.Public,
+                Token.Keyword.Modifier.Static,
+                Token.Type("Span"),
+                Token.Punctuation.TypeParameter.Begin,
+                Token.Type("T"),
+                Token.Punctuation.TypeParameter.End,
+                Token.Identifier.MethodName("CreateSpan"),
+                Token.Punctuation.TypeParameter.Begin,
+                Token.Identifier.TypeParameterName("T"),
+                Token.Punctuation.TypeParameter.End,
+                Token.Punctuation.OpenParen,
+                Token.Keyword.Modifier.Scoped,
+                Token.Keyword.Modifier.Ref,
+                Token.Type("T"),
+                Token.Identifier.ParameterName("reference"),
+                Token.Punctuation.Comma,
+                Token.PrimitiveType.Int,
+                Token.Identifier.ParameterName("length"),
+                Token.Punctuation.CloseParen,
+                Token.Punctuation.OpenBrace,
+                Token.Punctuation.CloseBrace
+            ]);
+        });
+
         it("expression body and type constraint (issue #74)", async () => {
             const input = Input.InClass(`
 T id1<T>(T a) => a;
